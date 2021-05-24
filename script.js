b72fa25024410a25e93a962529b4a06d9b43a213
Mr3O5 <46283095+Mr3O5@users.noreply.github.com>
2021-05-24 16:16:31 +0200

    javascript coding

---
+++ script.js
@@ -0,0 +1,7 @@
+function calculate(){
+
+    let number1 = +document.getElementById('number1').value;
+    let number2 = +document.getElementById('number2').value;
+    alert(number1 + number2);
+
+}
